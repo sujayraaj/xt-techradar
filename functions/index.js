4aeff6b4e27e7412ce52645d76b2621ce304b793
Vinci Rufus <vinci.rufus@gmail.com>
2017-12-07 16:35:50 +0530

    updating json with some data

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,17 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const express = require('express'),
+  path = require('path'),
+  fs = require('fs'),
+  app = express();
+PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'app')));
+const server = app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT);
+});
+
+app.get('/data', (req, res) => {
+  res.sendFile(__dirname + '/data/radar-config.json');
+});
