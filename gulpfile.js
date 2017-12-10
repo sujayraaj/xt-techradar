a3695e318e13c2678df5deba4c86d3ea63c434a7
Ranjan <kumarranjan.singh19@gmail.com>
2017-12-11 05:03:26 +0530

    build scripts for firebase deployment

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require("gulp");
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
+gulp.task("scripts", () => {
+  gulp
+    .src(["app/*.js"])
+    .pipe(
+      babel({
+        presets: ["env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest("dist"));
+});
+// Copy other files
+gulp.task("copy", () => {
+  gulp.src("app/*.css").pipe(gulp.dest("dist"));
+  gulp.src("app/*.html").pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy-data", () => {
+  gulp.src("data/*.json").pipe(gulp.dest("functions/data"));
+});
+
+gulp.task("default", ["scripts", "copy", "copy-data"], () => {});
